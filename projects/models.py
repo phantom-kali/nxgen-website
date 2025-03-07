ebe8180fd5ac74e507ec2ec68b9acca2612b7bfe
Victor Owino <vo4685336@gmail.com>
2025-03-08 00:36:02 +0300

    feat: add slug field to Project model and auto-generate slug from title

--- projects/models.py
+++ projects/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.text import slugify
 
 class Project(models.Model):
     title = models.CharField(max_length=255)
@@ -11,7 +12,13 @@ class Project(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_seeking_collaborators = models.BooleanField(default=False)
+    slug = models.SlugField(unique=True, blank=True)
     
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
     def __str__(self):
         return self.title
 
